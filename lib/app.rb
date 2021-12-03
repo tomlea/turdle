0d63866b9846b8539de008318e407a4adc05fdad
Tom Lea <commit@tomlea.co.uk>
2021-12-03 19:16:22 +0000

    Working

--- lib/app.rb
+++ lib/app.rb
@@ -1,7 +1,84 @@
-require 'sinatra'
+# frozen_string_literal: true
 
-set :root, File.expand_path("../..", __FILE__)
+require 'sinatra'
+require 'openssl'
+require 'json'
+
+set :root, File.expand_path('..', __dir__)
 
 get '/' do
+  game = Game.create
+  redirect "/g/#{game.to_blob}"
+end
+
+get '/g/:game' do
+  @game = Game.load(params[:game])
   erb :index
+end
+
+post '/g/:game' do
+  @game = Game.load(params[:game])
+  guess = params[:guess]
+  @game.guess(guess).to_json
+end
+
+SUPER_SECRET_KEY = Digest::SHA256.hexdigest('I suppose I first decided to be a panda when I was 12')[0, 32]
+
+class String
+  def encrypt
+    cipher = OpenSSL::Cipher.new('AES-256-CBC').encrypt
+    cipher.key = SUPER_SECRET_KEY
+    s = cipher.update(self) + cipher.final
+
+    s.unpack('H*')[0].upcase
+  end
+
+  def decrypt
+    cipher = OpenSSL::Cipher.new('AES-256-CBC').decrypt
+    cipher.key = SUPER_SECRET_KEY
+    s = [self].pack('H*').unpack('C*').pack('c*')
+
+    cipher.update(s) + cipher.final
+  end
+end
+
+class Game
+  def initialize(word)
+    @word = word
+  end
+
+  def self.create
+    new(words.sample)
+  end
+
+  def to_blob
+    @word.encrypt
+  end
+
+  def guess(word)
+    unless Game.words.include? word
+      return { error: "#{word} is not in the word list", word: word }
+    end
+    return { win: true, word: word, results: ['y', 'y', 'y', 'y', 'y', 'y'] } if word == @word
+
+    results =
+      word.split('').each_with_index.map do |l, index|
+        if @word[index] == l
+          'y'
+        elsif @word.include? l
+          'm'
+        else
+          'n'
+        end
+      end
+    { results: results, word: word }
+  end
+
+  def self.load(blob)
+    new(blob.decrypt)
+  end
+
+  def self.words
+    File.read(File.expand_path('../public/wordlist.txt', __dir__)).split("\n")
+  end
 end
